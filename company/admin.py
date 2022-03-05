b98474abaeec072027fcff246fffc5af2994beb3
ParwaanVirk <parwaanvirk@gmail.com>
2022-03-05 23:42:07 +0530

    init commit

---
+++ company/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from company.models import *
+# Register your models here.
+
+
+admin.site.register(CompanyS)
+admin.site.register(Forms)
+admin.site.register(Metrics)
+admin.site.register(Performance)
---
+++ login/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from login.models import Account
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+
+class AcconutAdmin(UserAdmin):
+    list_display = ('email', 'username', 'last_login', 'is_admin', )
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AcconutAdmin)
