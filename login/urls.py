445230fe81d7a0d115ff001a54faaed6431ee8c8
ashutoshc8101 <ashutoshc8101@gmail.com>
2022-03-18 20:49:39 +0530

    fixed registeration view

--- login/urls.py
+++ login/urls.py
@@ -4,8 +4,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name = 'register'), #not yet used
+    # TODO: Fix double login in the url.
     path('login/', obtain_auth_token, name = 'login'),  #this has been used
     path('UserData/', UserDataView.as_view(), name = 'UserData'),  #this has been used
     path('PasswordReset/', PasswordReset.as_view() ,name = "Change Password"),  #This has been used.
 ]
-    
